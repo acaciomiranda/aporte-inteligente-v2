10d694ab3185ca1107034f139cd0288524803462
Acácio Miranda <104951539+acaciomiranda@users.noreply.github.com>
2026-04-08 17:54:32 +0200

    Update quote.js

--- api/quote.js
+++ api/quote.js
@@ -1,4 +1,15 @@
 export default async function handler(req, res) {
+    // Permissões de CORS (Evita bloqueios de segurança do navegador em testes locais ou requisições externas)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+
+    // Lida com a requisição de pré-verificação (Preflight) do CORS
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
+
     const { codigo } = req.query;
     const token = process.env.BRAPI_TOKEN; // Puxa do cofre do Vercel
 
